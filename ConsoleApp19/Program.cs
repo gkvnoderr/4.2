77fc461484c1b17fb77d807acf2f97115aa36d26
gkvnoderr <123897793+gkvnoderr@users.noreply.github.com>
2023-10-30 20:38:02 +0300

    Add files via upload

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//Задание 1
+
+            //Console.WriteLine("Введите координату x первой точки");
+            //double x1 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите координату y первой точки");
+            //double y1 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите координату x второй точки");
+            //double x2 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите координату y второй точки");
+            //double y2 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите координату x третьей точки");
+            //double x3 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите координату y третьей точки");
+            //double y3 = double.Parse(Console.ReadLine());
+            //double dlina1 = Math.Sqrt(Math.Pow(x2 - x1, 2) - Math.Pow(y2 - y1, 2));
+            //double dlina2 = Math.Sqrt(Math.Pow(x3 - x2, 2) - Math.Pow(y3 - y2, 2));
+            //double dlina3 = Math.Sqrt(Math.Pow(x1 - x3, 2) - Math.Pow(y1 - y3, 2));
+            //double p = (dlina1 + dlina2 + dlina3);
+            //Console.WriteLine($"Периметр равен {p}");
+            //double s = Math.Sqrt(0.5 * (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1));
+            //Console.WriteLine($"Площадь равен {s}");
+
+            //Задание 2
+            
+
+            //Console.Write("Введите расстояние:");
+            //double s = double.Parse(Console.ReadLine());
+            //Console.Write("Введите расход бензина на 100 км:");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write("Введите стоимость бензина:");
+            //double c = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"Стоимость поездки:{s/100*b*c}руб");
+
+            //задание 3
+
+
+            //Console.WriteLine("Введите вес купленных конфет: ");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите вес купленного печенья: ");
+            //double y = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите вес купленных яблок: ");
+            //double z = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите стимость кг конфет: ");
+            //double x1 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите стоимость кг печенья: ");
+            //double y1 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите стоимость кг яблок: ");
+            //double z1 = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"Стоимость покупки составит {(x * x1) + (y * y1) + (z * z1)}руб.");
+            Console.ReadKey();
+        }
+    }
+}
